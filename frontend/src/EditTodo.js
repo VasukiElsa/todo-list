3d9ccb820170bbd32b625021f1171f168c5b05f1
VasukiElsa <vasuki.janarthanan6@gmail.com>
2024-01-20 23:04:12 +0530

    Make edit input field should be opened in alert and replace button with icon

--- frontend/src/EditTodo.js
+++ frontend/src/EditTodo.js
@@ -1,35 +1,31 @@
 import React, { useState } from 'react';
 import './TodoList.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
 
-const EditTodo = ({editTodo, todoId, initialText}) => {
-    const[editedText, setEditedText] = useState(initialText);
-    const[isEditing, setEditing] = useState(false);
+const EditTodo = ({ editTodo, todoId, initialText }) => {
+  const [editedText, setEditedText] = useState(initialText);
 
-    const handleSaveTodo = () => {
-        if(editedText.trim() !== ''){
-            editTodo(todoId, editedText);
-            setEditing(false);
-        }
-    };
+  const handleEditTodo = () => {
+    // Use window.prompt to display an alert with an input field
+    const userInput = window.prompt('Edit Todo:', initialText);
+    
+    // Check if the user clicked "Cancel" or entered an empty string
+    if (userInput === null || userInput.trim() === '') {
+      return;
+    }
 
+    // Save the edited todo
+    editTodo(todoId, userInput);
+  };
 
-    return(
-        <div>
-            <button onClick={() => setEditing(true)}>Edit</button>
-
-            {isEditing && (
-                <>
-                <input 
-                   className="input-edit" type="text" value={editedText}
-                   onChange={(e) => setEditedText(e.target.value)}
-                />
-
-                <button onClick={handleSaveTodo}>Save</button>
-                </>
-            )}
-        </div>
-    );
-
+  return (
+    <div>
+      <button className="editTodo-btn" onClick={handleEditTodo}>
+      <FontAwesomeIcon icon={faEdit} />
+      </button>
+    </div>
+  );
 };
 
 export default EditTodo;
