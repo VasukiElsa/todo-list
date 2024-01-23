8d6f1448dc7d04a4bb30e7b809fa8b3c562b35ce
VasukiElsa <vasuki.janarthanan6@gmail.com>
2024-01-24 05:17:37 +0530

    Add created_date field for timeStamp

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from datetime import datetime, timedelta
+
 
 class Todo(models.Model):
     text = models.CharField(max_length = 150)
+    created_date = models.DateTimeField(default=datetime.now)
+
     
     def __str__(self):
        return self.text
+
 
 
