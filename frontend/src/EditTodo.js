34539ddbfa7d7f5dac8c1762a6c349e42bf826da
VasukiElsa <vasuki.janarthanan6@gmail.com>
2024-01-24 05:24:08 +0530

    Modify edit ui to popover

--- frontend/src/EditTodo.js
+++ frontend/src/EditTodo.js
@@ -2,28 +2,45 @@ import React, { useState } from 'react';
 import './TodoList.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
-
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 const EditTodo = ({ editTodo, todoId, initialText }) => {
   const [editedText, setEditedText] = useState(initialText);
 
   const handleEditTodo = () => {
-    // Use window.prompt to display an alert with an input field
-    const userInput = window.prompt('Edit Todo:', initialText);
-    
-    // Check if the user clicked "Cancel" or entered an empty string
-    if (userInput === null || userInput.trim() === '') {
-      return;
-    }
 
-    // Save the edited todo
-    editTodo(todoId, userInput);
+    editTodo(todoId, editedText);
   };
 
   return (
     <div>
-      <button className="editTodo-btn" onClick={handleEditTodo}>
-      <FontAwesomeIcon icon={faEdit} />
-      </button>
+      <Popup 
+         trigger={
+          <button className="editTodo-btn">
+            <FontAwesomeIcon icon={faEdit}/>
+          </button>
+         }
+         position="right center"
+      >
+        {(close) => (
+          <div className="modal">
+            <div className="modal-content">
+              <textarea value={editedText} onChange={(e) => setEditedText(e.target.value)}/>
+            </div>
+
+            <div>
+              <button className = "edit-el-save" onClick={() =>{handleEditTodo(); close();}}>
+                Save
+              </button>
+
+              <button className = "edit-el-cancel" onClick={() => { close(); }}>
+                Cancel
+              </button>
+
+            </div>
+          </div>
+        )}
+      </Popup>
     </div>
   );
 };
